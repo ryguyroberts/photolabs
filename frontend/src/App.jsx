b3153c66d86f67a5933e12a749572e7691a48917
Ryan Robertson <ryguyrobertson@gmail.com>
2024-02-20 15:36:55 -0800

    add temp function to display 3 photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,11 +17,21 @@ const sampleDataForPhotoListItem = {
 };
 
 
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  // for now just a iterating loop
+  const createPhotos = (loops) => {
+    let photoArr = [];
+    for (let i = 1; i <= loops; i++) {
+      photoArr.push(<PhotoListItem key={i} photo={sampleDataForPhotoListItem}/>)
+    }
+    return photoArr;
+  }
+
   return (
     <div className="App">
-      <PhotoListItem photo={sampleDataForPhotoListItem}/>
+      {createPhotos(3)}
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -10,13 +10,13 @@ const PhotoListItem = (props) => {
       <img className="photo-list__image" src={props.photo.imageSource}/>
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={props.photo.profile}/>
-          <p className="photo-list__user-info">
+          <div className="photo-list__user-info">
             {props.photo.username}
             <br />
             <span className="photo-list__user-location">
               {props.photo.location.city}, {props.photo.location.country}
             </span>
-          </p>
+          </div>
       </div>
     </div>
   )
