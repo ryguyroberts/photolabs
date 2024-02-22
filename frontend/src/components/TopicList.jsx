5dd7c55efb7eaf5c11db623bc198db50785e3738
Ryan Robertson <ryguyrobertson@gmail.com>
2024-02-21 16:12:39 -0800

    update topics to use mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,24 +2,7 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from "../mocks/topics";
 
 const TopicList = () => {
 
@@ -34,7 +17,7 @@ const TopicList = () => {
 
   return (
     <div className="top-nav-bar__topic-list">
-      {createTopics(sampleDataForTopicList)}
+      {createTopics(topics)}
     </div>
   );
 };
